8914314cc4f5f778d256af687cd500c3776a8940
Charles Junior <charlesjunnr@gmail.com>
2023-03-14 13:23:27 -0300

    Atualizado.

---
+++ Matriz/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int linhas = 5;
+            int colunas = 5;
+            int[,] matriz2 = new int[linhas, colunas];
+
+            for(int i = 0; i < linhas; i++)
+            {
+                
+                for (int j = 0; j < colunas; j++)
+            {
+                    matriz2[i, j] = 0;
+            }
+           }
+           
+        }
+    }
+}
